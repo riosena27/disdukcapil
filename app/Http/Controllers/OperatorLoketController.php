60622a008040a73be2c42841da7d77ceec4b74fb
Achmad Fadhil <afadhilprtm@gmail.com>
2020-06-24 11:38:28 +0800

    operator loket

---
+++ app/Http/Controllers/OperatorLoketController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\AktaKelahiran;
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+
+class OperatorLoketController extends Controller
+{
+    public function index(){
+
+        $akta = AktaKelahiran::where('status_pengambilan', '<>', null)->where('tanggal_pengambilan', null)->get();
+
+        return view('operator_loket.index', [
+            'akta' => $akta
+        ]);
+    }
+
+    public function setPengambilan(AktaKelahiran $akta, Request $request){
+        $operator = Auth::user()->id;
+        $tanggalPengambilan = new Carbon($request->tanggal_pengambilan);
+        $akta->tanggal_pengambilan = $tanggalPengambilan;
+        $akta->operator_loket_id = $operator;
+        $akta->save();
+
+        return back()->with('success', 'Tanggal pengambilan berhasil di set');
+    }
+}
